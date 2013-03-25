222e4d18cd91466a2555c72057b682cab6e95f65
Mael Costantini <zozor92@hotmail.fr>
2013-03-24 21:08:14 -0400

    ajout class CarCouleur

--- Memory/src/Carte.java
+++ Memory/src/Carte.java
@@ -78,58 +78,6 @@ public abstract class Carte extends JComponent{
 
 
 
-	public class CarteCouleur extends Carte{
-
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 3258550638779901058L;
-		private Color couleur;
-
-		public CarteCouleur(boolean sens,Color couleur){
-			super(sens);
-			this.couleur=couleur;
-		}
-
-		public CarteCouleur(CarteCouleur s){
-			super(s);
-			this.couleur=s.couleur;
-		}
-
-
-		public void paintRecto(Graphics2D g){
-			
-			
-		}
-
-		public Object getCaract(){
-			return this.couleur;
-		}
-
-
-		/**
-		 * Methode qui retourne la donnée caractéristique de l'élément
-		 * @param s la carte en question
-		 */
-		public boolean rectoIdentique(Carte s){
-			if(this.couleur==s.getCaract()){
-				return true;
-			}else{
-				return false;
-			}
-		}
-
-		public String toString(){
-			return "CarteCouleur: "+this.couleur;
-		}
-
-		public Object duplique(){
-			CarteCouleur a = new CarteCouleur(this);
-			return a;
-		}
-
-
-	}
 
 	/*public class CarteMot extends Carte{
 		private static final long serialVersionUID = 1L;
---
+++ Memory/src/CarteCouleur.java
@@ -0,0 +1,56 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+	public class CarteCouleur extends Carte{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 3258550638779901058L;
+		private Color couleur;
+
+		public CarteCouleur(boolean sens,Color couleur){
+			super(sens);
+			this.couleur=couleur;
+		}
+
+		public CarteCouleur(CarteCouleur s){
+			super(s);
+			this.couleur=s.couleur;
+		}
+
+
+		public void paintRecto(Graphics2D g){
+			
+			
+		}
+
+		public Object getCaract(){
+			return this.couleur;
+		}
+
+
+		/**
+		 * Methode qui retourne la donnée caractéristique de l'élément
+		 * @param s la carte en question
+		 */
+		public boolean rectoIdentique(Carte s){
+			if(this.couleur==s.getCaract()){
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+		public String toString(){
+			return "CarteCouleur: "+this.couleur;
+		}
+
+		public Object duplique(){
+			CarteCouleur a = new CarteCouleur(this);
+			return a;
+		}
+
+
+	}
+
