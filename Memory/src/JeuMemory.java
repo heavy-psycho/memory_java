35b078ba23704ac0ed07e26cd78ce9af951c2473
Alexandre <heavy-psycho@hotmail.fr>
2013-04-03 00:17:40 -0400

    update

--- Memory/src/JeuMemory.java
+++ Memory/src/JeuMemory.java
@@ -6,7 +6,7 @@ public class JeuMemory {
 	public static void main(String[] args) {
 		Carte[] a = new CarteCouleur[8];
 		GenerateurDeCartesCouleur gen = new GenerateurDeCartesCouleur();
-		a=gen.generePairesDeCartesMelangees(4);
+		a=gen.generePairesDeCartesMelangees(32);
 		PanneauDeCartes b = new PanneauDeCartes(4,5,a,5,5);
 	}
 }
